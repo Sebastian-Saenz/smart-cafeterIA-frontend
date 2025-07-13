63703c5d3282a16ff7de6fd99cc1e50138723875
Sebastian Saenz <sebastiansaenz77spam@gmail.com>
2025-07-13 17:00:29 +0000

    vercel google test

--- static/js/oauthSignIn.js
+++ static/js/oauthSignIn.js
@@ -2,16 +2,20 @@ function oauthSignIn() {
   const redirect_base = document.querySelector('meta[name="redirect-uri"]')
     .getAttribute('content');
 
+  const state = 'random_' + Math.random().toString(36).substr(2);
+  console.log('🔐 Generado state desde oauthSignIn:', state);
+
   const params = {
     client_id: '401510539727-kmju5p4gsbitskfkvu6q4cmr2a0ht3ng.apps.googleusercontent.com',
     redirect_uri: redirect_base,
     response_type: 'code',
     scope: 'openid email profile',
     include_granted_scopes: 'true',
-    state: 'random_' + Math.random().toString(36).substr(2)
+    state: state
   };
   // Guarda el state para verificar luego
   localStorage.setItem('oauth_state', params.state);
+
   const url = 'https://accounts.google.com/o/oauth2/v2/auth?' + new URLSearchParams(params);
   window.location = url;
 }
